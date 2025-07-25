f5ada425c503df9a9c224e8646b96791671586ec
Kleo <kleobytyci12@gmail.com>
2025-07-25 10:58:07 +0100

    feat: add class-based dense layer and integrate spiral dataset for testing

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,15 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import nnfs
+from nnfs.datasets import spiral_data
 
-inputs = [1, 2, 3, 2.5]
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+        
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-weights = [[0.2, 0.8, -0.5, 1],[0.5, -0.91, 0.26, -0.5],[-0.26, -0.27, 0.17, 0.87]]
-
-biases = [2, 3, 0.5]
+nnfs.init()
 
 
+X, y = spiral_data(samples=100, classes=3)
+plt.scatter(X[:, 0],X[:, 1], c=y, cmap='brg')
+plt.show()
 
-outputs = np.dot(weights, inputs)+biases
+dense1 = Layer_Dense(2, 3)
+dense1.forward(X)
 
-
-
-print(outputs)
+print(dense1.output[:5])
