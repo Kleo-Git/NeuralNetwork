42d45b6fd561f8f9ffcbc08f9d77ad79007b8078
Kleo <kleobytyci12@gmail.com>
2025-07-30 20:33:00 +0100

    feat: add loss calculation with categorical cross-entropy

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,12 +5,14 @@ from nnfs.datasets import spiral_data
 
 class Activation_ReLU:
     def forward(self, inputs):
+        
         #Applies the ReLU activation function
         #Replaces all negative values with 0, positive values remain unchanged
         self.output = np.maximum(0, inputs)
         
 class Activation_Sigmoid:
     def forward(self, inputs):
+        
         #Applies the Sigmoid activation function
         #Returns input values in the range (0,1) using a curve
         #Curve rapidly changes near 0, smoothes out at larger values
@@ -18,28 +20,72 @@ class Activation_Sigmoid:
         
 class Activation_Step:
     def forward(self, inputs):
+        
         #Applies the Step activation function
         #Returns 1 for inputs >= 0, otherwise returns 0
         self.output = np.where(inputs >= 0, 1, 0)
         
 class Activation_Softmax:
     def forward(self, inputs): 
+        
         #Calculate the unormalized probabilties
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        
         #Normalize the probabilities for each sample
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        
         self.output = probabilities
         
+class Loss:
+    #Calculates the data and regularization losses
+    def calculate(self, output, y):
+        
+        #Calculates the sample losses
+        sample_losses = self.forward(output, y)
+        
+        #Calculate mean loss
+        data_loss = np.mean(sample_losses)
+        
+        #Return mean loss
+        return data_loss
+    
+class Loss_CategoricalCrossEntropy(Loss):
+    
+    #Forward Pass
+    def forward(self, y_predicted, y_actual):
+        
+        #Number of samples in a batch
+        samples = len(y_predicted)
+        
+        #Clip data to prevent division by 0, and avoid shifting mean towards a value
+        y_predicted_clipped = np.clip(y_predicted, 1e-7, 1 - 1e-7)
+        
+        #Probabilities for target values
+        #If labels are sparse
+        if len(y_actual.shape) == 1:
+            correct_confidences = y_predicted_clipped[range(samples), y_actual]
+            
+        #If labels are one-hot encoded
+        elif len(y_actual.shape) == 2:
+            correct_confidences = np.sum(y_predicted_clipped * y_actual, axis = 1)
+            
+        #Calculate the loss value
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+            
 
 # Dense (fully connected) layer class
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
+        
         #Initialize weights with small random values
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        
         #Initialize biases with zeroes
         self.biases = np.zeros((1, n_neurons))
         
     def forward(self, inputs):
+        
         #Compute the output of a foward pass of a layer
         self.output = np.dot(inputs, self.weights) + self.biases
 
@@ -67,6 +113,9 @@ dense2 = Layer_Dense(3,3)
 #Create a softmax activation
 activation2 = Activation_Softmax()
 
+# Create loss function
+loss_function = Loss_CategoricalCrossEntropy()
+
 #Perform a forward pass of data through the layer
 dense1.forward(X)
 
@@ -81,8 +130,15 @@ dense2.forward(activation1.output)
 #Make a forward pass through the softmax activation function
 activation2.forward(dense2.output)
 
-
 print(activation2.output[:5])
+
+#Forward pass through activation function
+#Takes output of second dense layer and returns loss
+loss = loss_function.calculate(activation2.output, y)
+
+print('loss:', loss)
+
+
 
 
 
